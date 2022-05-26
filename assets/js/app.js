86eb7cf2eeb67d354490efacf5f5e5e4a6fe5687
ydk-tulsi <ydktulsi@gmail.com>
2022-05-26 19:46:51 +0530

    second commit

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+ let cl = console.log;
+
+ 
+ const navigation = document.getElementById("navigation");
+ const navToggle = document.getElementById('navToggle');
+
+ navToggle.addEventListener('click', function(){
+     navigation. classList. toggle('active')
+
+
+ })
